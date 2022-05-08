845d88bb99cd093027bd1c296309e3225350c79a
Mariusz <mariusz_sielicki@o2.pl>
2022-05-08 12:06:56 +0200

    argumenty prymitywne i zlozone metod

--- src/main/java/dz2_02_typyIArgumentyMetod.java
+++ src/main/java/dz2_02_typyIArgumentyMetod.java
@@ -8,10 +8,13 @@ public class dz2_02_typyIArgumentyMetod {
     public static void main(String[] args) {
         Pracownik programista1;
         Pracownik kontroler1;
+        Pracownik glazurnik1;
 
         programista1 = new Pracownik(); //nastepny programist to bylby np programista2
         kontroler1 = new Pracownik();
+        glazurnik1 = new Pracownik();
 
+//patrz gettery w definicji klasy
         programista1.imie = "Adam";
         programista1.nazwisko = "Adamowski";
         programista1.wiek = 22;
@@ -20,10 +23,16 @@ public class dz2_02_typyIArgumentyMetod {
         kontroler1.nazwisko = "Kasinska";
         kontroler1.wiek = 24;
 
+//patrz settery w definicji klasy
+        glazurnik1.setImie("Boguslaw");
+        glazurnik1.setNazwisko("Lecina");
+        glazurnik1.setWiek(45);
+
 //        System.out.println(programista1.imie +" "+ programista1.wiek);
 
         programista1.wyswietlDane();
         kontroler1.wyswietlDane();
+        glazurnik1.wyswietlDane();
     }
 }
 class Pracownik{
@@ -34,7 +43,7 @@ class Pracownik{
     public void wyswietlDane() {
         System.out.println("Pracownik: "+imie+" "+nazwisko+"\t"+wiek+" lat");
     }
-
+// gettery
     public String getImie() {
         return imie;
     }
@@ -45,5 +54,17 @@ class Pracownik{
 
     public int getWiek() {
         return wiek;
+    }
+// settery
+    public void setWiek (int wiek){
+       this.wiek = wiek;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
     }
 }
---
+++ src/main/java/dz2_03_typyObiektowe_Punkt.java
@@ -0,0 +1,36 @@
+public class dz2_03_typyObiektowe_Punkt {
+
+    /*
+roznica miedzy argumentem metody typu
+    - prymitywnego (int, char, boolean, ... )
+    - zlozonego - czyli obiektu klasy - tutaj klasy Punkt
+ */
+
+
+    public static void main(String[] args) {
+        Punkt p1 = new Punkt();
+        p1.x = 10;
+        p1.y = 1;
+
+        zmienPunkt(p1);  //metoda zmienPunkt operuje i zmienia obiekt p1
+        System.out.println(p1.x + "," + p1.y); ////da 11,0
+
+        int a = 10;
+        zmienA(a); // metoda zmienA operuje na kopii wartosci zmiennej a
+        System.out.println(a); //zwroci 10
+    }
+
+    static void zmienPunkt(Punkt punkt) {
+        punkt.x++;
+        punkt.y--;
+    }
+
+    static int zmienA(int a) {
+        return a = a++;
+    }
+}
+
+class Punkt {
+    int x;
+    int y;
+}
